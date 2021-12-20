510c171ed2a97a30cf74f981a1532387949492df
SheilaNS <shei.nsantos@gmail.com>
2021-12-20 18:29:09 -0300

    Requisito 16 ok

--- script.js
+++ script.js
@@ -2,6 +2,23 @@ const botaoCriarCarta = document.getElementById('criar-carta'); // button
 const textoCarta = document.getElementById('carta-texto'); // input
 const textoGerado = document.getElementById('carta-gerada'); // p
 const carta = textoGerado.childNodes;
+const estilo = ['newspaper', 'magazine1', 'magazine2'];
+const tamanho = ['medium', 'big', 'reallybig'];
+const rotacao = ['rotateleft', 'rotateright'];
+const inclinacao = ['skewleft', 'skewright'];
+
+function escolheIndice(array) {
+  const tamanhoArray = array.length;
+  const indiceEscolhido = Math.floor(Math.random() * tamanhoArray);
+  return array[indiceEscolhido];
+}
+
+function escolheLista(param) {
+  param.classList.add(escolheIndice(estilo));
+  param.classList.add(escolheIndice(tamanho));
+  param.classList.add(escolheIndice(rotacao));
+  param.classList.add(escolheIndice(inclinacao));
+}
 
 function resetaCarta() {
   for (let i = carta.length; i > 0; i -= 1) {
@@ -11,6 +28,10 @@ function resetaCarta() {
   }
 }
 
+/* function escolheDeNovo() {
+
+}
+ */
 function geraCarta() {
   resetaCarta();
   let palavras = [];
@@ -18,6 +39,8 @@ function geraCarta() {
   for (let i = 0; i < palavras.length; i += 1) {
     const span = document.createElement('span');
     span.innerText = palavras[i];
+    escolheLista(span);
+    // span.addEventListener('click', escolheDeNovo);
     textoGerado.appendChild(span);
   }
 }
